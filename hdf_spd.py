9aa26d8fce1c7a5734b044f86d323f6a84e82da7
Zhu Di <you@example.com>
2017-06-26 00:14:49 +0800

    2

--- hdf_spd.py
+++ hdf_spd.py
@@ -15,30 +15,33 @@ def get_page(url):
     return r.content
 
 #解析地区省市页面
-def parse_province(province, url):
+def parse_province(province_name, url):
     soup = BeautifulSoup(get_page(url), 'lxml')
     m_ctt_green_s = soup.select('div[class="m_ctt_green"]')
     hospital_list = []
     for m_ctt_green in m_ctt_green_s:
+        #市级名称
         city = m_ctt_green.previous_sibling.previous_sibling.string
         hospitals = m_ctt_green.select('a')
         for hospital in hospitals:
+            #医院链接
             hospital_href = 'http://www.haodf.com/' + hospital['href']
+            #医院简称
             hospital_name1 = hospital.string
-            hospital_list.append((hospital_name1, hospital_href, province, city))
+            hospital_list.append((hospital_name1, hospital_href, province_name, city))
     return hospital_list
 
 
 r = get_page(base_url)
-soup = BeautifulSoup(r, 'lxml')
+bj_soup = BeautifulSoup(r, 'lxml')
 
 province = []
-bj = soup.select('div[class="kstl2"]')[0].a.string
-bj_href = soup.select('div[class="kstl2"]')[0].a['href']
+bj = bj_soup.select('div[class="kstl2"]')[0].a.string
+bj_href = bj_soup.select('div[class="kstl2"]')[0].a['href']
 province.append((bj, bj_href))
 
 #获取各省链接
-for kstl in soup.select('div[class="kstl"]'):
+for kstl in bj_soup.select('div[class="kstl"]'):
     province_name = kstl.a.string
     province_href = kstl.a['href']
     province.append((province_name, province_href))
@@ -49,6 +52,12 @@ for province, url in province:
         page = get_page(hospital[1])
         time.sleep(1)#续1秒！
         hospital_soup = BeautifulSoup(page, 'lxml')
+        # 获取医院全名
+        hospital_name = hospital_soup.select('div[id="ltb"] span a')[0].string
+        print(hospital_name)
+        # 获取医院等级
         p = hospital_soup.select('div[id="contentA"] div[class="toptr"] p')[0]
-        if p.text.find('('):
-            compile = re.compile('.*?\(()\)')
+        if '(' in p.text:
+            pattern = re.compile('\((.*)\)')
+            hospital_levle = pattern.findall(p.text)[0]
+
