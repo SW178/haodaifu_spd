21113df41c612afb3a5308c9f7ef7c7e36e3c685
zhudi <zhuuu1234@gmail.com>
2017-06-30 17:58:23 +0800

    1

--- hdf_spd.py
+++ hdf_spd.py
@@ -1,11 +1,35 @@
 # -*- coding:utf-8 -*-
 import time
+import pymysql
 import re
 import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 base_url = 'http://www.haodf.com/yiyuan/all/list.htm'
+
+db = pymysql.connect('localhost', 'root', 'a555554444', 'DOCTOR')
+# 使用cursor()方法获取操作游标
+cursor = db.cursor()
+
+# 使用 execute() 方法执行 SQL，如果表存在则删除
+cursor.execute("DROP TABLE IF EXISTS DOCTOR")
+# 使用预处理语句创建表
+sql = """CREATE TABLE DOCTOR (
+            ID int NOT NULL,
+            DOCTOR_NAME CHAR(100),
+            TITLE CHAR(100),
+            DOCTOR_HREF CHAR(200),
+            DEPARTMENT CHAR(100),
+            HOSPITAL_NAME CHAR(100),
+            HOSPITAL_LEVEL CHAR(50),
+            CITY CHAR(100),
+            PROVINCE CHAR(100),
+            PRIMARY KEY (ID)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;"""
+
+cursor.execute(sql)
+db.close()
 
 def get_page(url):
     ua = UserAgent()
@@ -18,7 +42,6 @@ def get_page(url):
 def parse_province(province_name, url):
     soup = BeautifulSoup(get_page(url), 'lxml')
     m_ctt_green_s = soup.select('div[class="m_ctt_green"]')
-    hospital_list = []
     for m_ctt_green in m_ctt_green_s:
         #市级名称
         city = m_ctt_green.previous_sibling.previous_sibling.string
@@ -49,6 +72,30 @@ def parse_department(url):
         doctor_href = li.a["href"]
         yield doctor_name, doctor_title, doctor_href
 
+#保存至数据库
+def add_to_sql(doctor_name, title, doctor_href, department, hospital_name, hospital_level, city, province):
+    print(doctor_name, title, doctor_href, department, hospital_name, hospital_level, city, province)
+    # 打开数据库连接
+    db = pymysql.connect('localhost', 'root', 'a555554444', 'DOCTOR')
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+    # SQL 插入语句
+    sql = "INSERT INTO DOCTOR(DOCTOR_NAME, TITLE, DOCTOR_HREF, DEPARTMENT, HOSPITAL_NAME, HOSPITAL_LEVEL, CITY, \
+    PROVINCE) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)" % (doctor_name, title, doctor_href, department, hospital_name, hospital_level, city, province)
+
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 执行sql语句
+        db.commit()
+        print('保存成功！')
+    except:
+        # 发生错误时回滚
+        db.rollback()
+        print(db.Error)
+    # 关闭数据库连接
+    db.close()
+
 r = get_page(base_url)
 bj_soup = BeautifulSoup(r, 'lxml')
 
@@ -67,6 +114,7 @@ for kstl in bj_soup.select('div[class="kstl"]'):
 for province, url in province:
     #遍历医院
     for hospital in parse_province(province, url):
+        city = hospital[3]
         page = get_page(hospital[1])
         # time.sleep(1)#续1秒！
         hospital_soup = BeautifulSoup(page, 'lxml')
@@ -77,14 +125,15 @@ for province, url in province:
         p = hospital_soup.select('div[id="contentA"] div[class="toptr"] p')[0]
         if '(' in p.text:
             pattern = re.compile('\((.*)\)')
-            hospital_levle = pattern.findall(p.text)[0]
-            hospital.append(hospital_levle)
+            hospital_level = pattern.findall(p.text)[0]
+            hospital.append(hospital_level)
         else:
-            hospital.append('')
+            hospital_level = ''
+            hospital.append(hospital_level)
         #科室列表页面url
         department_url = hospital[1][:-4] + '/keshi.htm'
         #遍历科室列表页面
         for department_name, department_href in parse_departmentlist(department_url):
             #遍历医生列表页面
             for doctor in parse_department(department_href):
-                print(doctor)
+                add_to_sql(doctor[0], doctor[1], doctor[2], department_name, hospital_name, hospital_level, city, province)
